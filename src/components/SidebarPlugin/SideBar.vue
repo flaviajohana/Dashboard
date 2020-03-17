7610c37557eb8c8ef31667cd2a5420e3d66754a6
Flavia Johana <flavia.johana11@hotmail.com>
2020-03-16 21:28:47 -0400

    Atualização de dados json para alimentar dashboard; Adição de logo da empresa.

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -13,7 +13,7 @@
       <div class="logo">
         <a href="#" class="simple-text">
           <div class="logo-img">
-            <img src="@/assets/img/vue-logo.png" alt="" />
+            <img src="@/assets/img/infostore.jpg" alt="" />
           </div>
           {{ title }}
         </a>
