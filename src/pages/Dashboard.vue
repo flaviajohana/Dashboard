97db19e6de4cfa9dd90689417459f7670595026c
Patrick <patrick.tapajos@gmail.com>
2020-03-18 18:40:29 -0400

    Alterações em layout de ícones.

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -12,16 +12,11 @@
 
     <!--Stats cards-->
     <div class="row">
-      <div
-        class="col-md-6 col-xl-2"
-        v-for="(opcao, index) in cards"
-        :key="opcao.id"
-      >
+      <div class="col-md-6 col-xl-2" v-for="(opcao, index) in cards" :key="opcao.id">
         <stats-card>
           <div class="icon-big text-center" slot="header">
             <i>
               <img :src="icones[index].icone" :alt="opcao.descricao" />
-  
             </i>
           </div>
           <div class="numbers" slot="content">
@@ -35,24 +30,24 @@
     <!--Charts-->
     <div class="row">
       <div class="col-12">
-        <apexchart width="45%" :options="chartOptions" :series="series">
-        </apexchart>
+        <apexchart width="45%" :options="chartOptions" :series="series"></apexchart>
       </div>
     </div>
     <div id="id_legenda" class="legend">
       <ul>
         <li class="header">Legenda</li>
         <li class="treeview">
-          <i class="list-group-item-inline promotor"></i>
+          <img src="/imagens/emogis-05.png" alt="promotor" />
           <span>Promotor</span>
-	      </li>
+        </li>
         <li class="treeview">
-          <i class="list-group-item-inline neutro"></i>
+          <img src="/imagens/emogis-04.png" alt="neutro" />
           <span>Neutro</span>
+        </li>
         <li class="treeview">
-          <i class="list-group-item-inline detrator"></i>
+          <img src="/imagens/emogis-02.png" alt="detrator" />
           <span>Detrator</span>
-	      </li>
+        </li>
       </ul>
     </div>
   </div>
@@ -60,7 +55,7 @@
 <script>
 import { StatsCard } from "@/components/index";
 
-const API_URL_LISTAR = process.env.VUE_APP_URL+"api/v4/frete/feedback/stats";
+const API_URL_LISTAR = process.env.VUE_APP_URL + "api/v4/frete/feedback/stats";
 
 export default {
   components: {
@@ -73,25 +68,28 @@ export default {
     return {
       totalParticipantes: 1000,
       pergunta: "",
-      traducaoPerg:[
-        {id:"", descricao:"Todos"},
-        {id:"price", descricao:"Preço"},
-        {id:"product_mix", descricao:"Mix Produto"},
-        {id:"customer_service", descricao:"Atendimento ao Cliente"},
-        {id:"delivery_speed", descricao:"Rapidez na Entrega"},
-        {id:"pleasant_environment", descricao:"Ambiente Agradável"},
-        {id:"sales_consultants", descricao:"Consultores de Vendas"},
-        {id:"service_team", descricao:"Equipe de Serviço"},
-        {id:"cash_team", descricao:"Equipe de Caixa"},
-        {id:"delivery_team", descricao:"Equipe de Entrega"},
-        {id:"customer_expectations", descricao:"Expectativas do Cliente"},
-        {id:"new_purchases", descricao:"Novas Compras"},
-        {id:"recommendation", descricao:"Recomendação"},
-        
+      traducaoPerg: [
+        { id: "", descricao: "Todos" },
+        { id: "price", descricao: "Preço" },
+        { id: "product_mix", descricao: "Mix Produto" },
+        { id: "customer_service", descricao: "Atendimento ao Cliente" },
+        { id: "delivery_speed", descricao: "Rapidez na Entrega" },
+        { id: "pleasant_environment", descricao: "Ambiente Agradável" },
+        { id: "sales_consultants", descricao: "Consultores de Vendas" },
+        { id: "service_team", descricao: "Equipe de Serviço" },
+        { id: "cash_team", descricao: "Equipe de Caixa" },
+        { id: "delivery_team", descricao: "Equipe de Entrega" },
+        { id: "customer_expectations", descricao: "Expectativas do Cliente" },
+        { id: "new_purchases", descricao: "Novas Compras" },
+        { id: "recommendation", descricao: "Recomendação" }
       ],
-      traducaoOpc: 
-         {"pessimo":"Péssimo", "insatisfeito":"Insatisfeito", "neutro":"Neutro", "bom":"Bom", "excelente":"Excelente"}
-      ,
+      traducaoOpc: {
+        pessimo: "Péssimo",
+        insatisfeito: "Insatisfeito",
+        neutro: "Neutro",
+        bom: "Bom",
+        excelente: "Excelente"
+      },
       chartOptions: {
         plotOptions: {
           pie: {
@@ -100,7 +98,7 @@ export default {
             offsetY: 0,
             dataLabels: {
               minAngleToShowLabel: 1
-          }, 
+            }
           }
         },
         title: {
@@ -135,11 +133,7 @@ export default {
           }
         ],
         fill: {
-          colors: [
-            "rgb(50,205,50)",
-            "rgb(255,255,0)",
-            "rgb(255,0,0)"
-          ]
+          colors: ["rgb(50,205,50)", "rgb(255,255,0)", "rgb(255,0,0)"]
         }
       },
       series: [],
@@ -147,29 +141,22 @@ export default {
         {
           id: 1,
           icone: "/imagens/emogis-02.png"
-            
         },
         {
           id: 2,
-          icone:"/imagens/emogis-03.png"
-          
-            
+          icone: "/imagens/emogis-03.png"
         },
         {
           id: 3,
-          icone:"/imagens/emogis-04.png"
-          
-            
+          icone: "/imagens/emogis-04.png"
         },
         {
           id: 4,
-          icone:"imagens/emogis-05.png"
-          
+          icone: "imagens/emogis-05.png"
         },
         {
           id: 5,
-          icone:"imagens/emogis-06.png"
-           
+          icone: "imagens/emogis-06.png"
         }
       ],
       cards: [],
@@ -189,40 +176,43 @@ export default {
       let neutro = 0;
       let detrator = 0;
 
-      if(this.pergunta == ""){
-        Object.keys(this.perguntaList).forEach(function(item){
+      if (this.pergunta == "") {
+        Object.keys(this.perguntaList).forEach(function(item) {
           let total = 0;
-          Object.keys(self.perguntaList[item]).forEach(function(inner){
+          Object.keys(self.perguntaList[item]).forEach(function(inner) {
             total += self.perguntaList[item][inner];
           });
-          if(item == "excelente" || item == "bom"){
+          if (item == "excelente" || item == "bom") {
             promotor += total;
-          }else if(item == "neutro"){
+          } else if (item == "neutro") {
             neutro += total;
-          }else if(item == "pessimo" || item == "insatisfeito"){
+          } else if (item == "pessimo" || item == "insatisfeito") {
             detrator += total;
           }
-          self.cards.push({descricao: self.traducaoOpc[item], valor: total });
+          self.cards.push({ descricao: self.traducaoOpc[item], valor: total });
         });
-      }else{
-        Object.keys(this.perguntaList).forEach(function(item){
+      } else {
+        Object.keys(this.perguntaList).forEach(function(item) {
           let total = 0;
-          Object.keys(self.perguntaList[item]).forEach(function(inner){
-            if(inner == self.pergunta){
+          Object.keys(self.perguntaList[item]).forEach(function(inner) {
+            if (inner == self.pergunta) {
               total += self.perguntaList[item][inner];
-              self.cards.push({descricao: self.traducaoOpc[item], valor: total });
+              self.cards.push({
+                descricao: self.traducaoOpc[item],
+                valor: total
+              });
             }
           });
-          if(item == "excelente" || item == "bom"){
-              promotor += total;
-            }else if(item == "neutro"){
-              neutro += total;
-            }else if(item == "pessimo" || item == "insatisfeito"){
-              detrator += total;
-            }
+          if (item == "excelente" || item == "bom") {
+            promotor += total;
+          } else if (item == "neutro") {
+            neutro += total;
+          } else if (item == "pessimo" || item == "insatisfeito") {
+            detrator += total;
+          }
         });
       }
-      
+
       this.chartOptions.labels.push("promotor");
       this.chartOptions.labels.push("neutro");
       this.chartOptions.labels.push("detrator");
@@ -246,7 +236,7 @@ export default {
       var self = this;
       this.axios.get(API_URL_LISTAR, {}).then(function(resultado) {
         self.perguntaList = resultado.data.result;
-        console.log(resultado)
+        console.log(resultado);
         self.updateCardsAndChart();
         // self.loadQuestions();
       });
@@ -264,25 +254,29 @@ export default {
 };
 </script>
 <style>
-ul{
+ul {
   list-style-type: none;
 }
 .list-group-item-inline {
-    float: left;
-    width: 20px;
-    height: 20px;
-    margin-right: 5px;
-    /* border-radius: 3px; */
-    border-radius: 15px;
-    border: 1px solid none;
+  float: left;
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+  /* border-radius: 3px; */
+  border-radius: 15px;
+  border: 1px solid none;
 }
-.detrator{
-    background-color: rgba(255,0,0) !important;
+.detrator {
+  background-color: rgba(255, 0, 0) !important;
 }
 .promotor {
-    background-color: rgba(50,205,50) !important;
+  background-color: rgba(50, 205, 50) !important;
 }
-.neutro  {
-    background-color: rgba(255,255,0) !important;
+.neutro {
+  background-color: rgba(255, 255, 0) !important;
+}
+
+.icon-big > img {
+  width: 50px;
 }
 </style>
